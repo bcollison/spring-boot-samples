ce0b745d1fc02bf1996fc7bccbf0e954ec3c653d
Brian Collison <brian@verve.com>
2021-03-31 16:27:28 -0700

    only mock the joke service

--- catalog-service/src/test/java/com/briancollison/sbdemo/controller/WidgetRestTest.java
+++ catalog-service/src/test/java/com/briancollison/sbdemo/controller/WidgetRestTest.java
@@ -1,7 +1,6 @@
 package com.briancollison.sbdemo.controller;
 
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,43 +11,57 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.briancollison.sbdemo.dao.WidgetRepository;
 import com.briancollison.sbdemo.model.Joke;
 import com.briancollison.sbdemo.model.Widget;
-import com.briancollison.sbdemo.model.WidgetDto;
 import com.briancollison.sbdemo.service.JokeService;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class WidgetRestTest {
+class WidgetRestTest {
     @Autowired
     MockMvc mockMvc;
 
     @MockBean
     JokeService jokeService;
 
-    @MockBean
+    @Autowired
     WidgetRepository widgetRepository;
 
-    @MockBean
-    ModelMapper modelMapper;
+    @Test
+    void test_succesful_get_all() throws Exception {
+        for (int i = 1; i < 6; i++) {
+            widgetRepository.save(buildWidget(i));
+        }
+
+        this.mockMvc
+                .perform(get("/widgets")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$", hasSize(5)));
+    }
+
+    private Widget buildWidget(int i) {
+        Widget w = new Widget();
+        w.setId(i);
+        w.setName("Name: " + i);
+        w.setDescription("Description: " + i);
+        w.setThumbnailUrl("Thumbnail: " + i);
+        return w;
+    }
 
     @Test
     void test_successful_save() throws Exception {
         Joke joke = new Joke();
         joke.setValue("i am funny");
-        Widget widget = new Widget();
-        widget.setName("name");
-        widget.setDescription("joke");
 
         when(jokeService.getJoke()).thenReturn(joke);
-        when(widgetRepository.save(isA(Widget.class))).thenReturn(widget);
-        when(modelMapper.map(isA(WidgetDto.class), eq(Widget.class))).thenReturn(widget);
+
         this.mockMvc
                 .perform(post("/widgets")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -56,9 +69,27 @@ public class WidgetRestTest {
                                 "  \"name\": \"name\"\n" +
                                 "}"))
                 .andExpect(status().is(201))
-                .andExpect(jsonPath("$.description", is("joke")))
+                .andExpect(jsonPath("$.description", is("i am funny")))
                 .andExpect(jsonPath("$.name", is("name")));
-
-
     }
+
+    @Test
+    void test_successful_save_with_description() throws Exception {
+        Joke joke = new Joke();
+        joke.setValue("i am funny");
+
+        when(jokeService.getJoke()).thenReturn(joke);
+
+        this.mockMvc
+                .perform(post("/widgets")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "  \"name\": \"name\", \"description\":\"desc\",\"thumbnailUrl\":\"tu\"\n" +
+                                "}"))
+                .andExpect(status().is(201))
+                .andExpect(jsonPath("$.description", is("desc")))
+                .andExpect(jsonPath("$.thumbnailUrl", is("tu")))
+                .andExpect(jsonPath("$.name", is("name")));
+    }
+
 }
